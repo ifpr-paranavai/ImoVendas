56ac330f3c20bbb0fec672f8c1ea4bac08c0600c
jgdml <jgdml2801@gmail.com>
2022-06-20 20:44:45 -0300

    detectar e salvar usuário logado no imóvel

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,13 +1,22 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Imovel
 
 # Create your views here.
 
 
-class ImovelCreate(CreateView):
+class ImovelCreate(LoginRequiredMixin, CreateView):
     model = Imovel
     fields = ["titulo", "descricao", "preco", "cep", "rua", "bairro", "numero",
-              "cidade", "quantidade_quartos", "quantidade_banheiros", "area", "tipo", "finalidade", "usuario",]
+              "cidade", "quantidade_quartos", "quantidade_banheiros", "area", "tipo", "finalidade"]
     template_name = "cadastros/imovel-form.html"
     success_url = reverse_lazy('index')
+
+    def form_valid(self, form):
+
+        form.instance.usuario = self.request.user
+
+        url = super().form_valid(form)
+
+        return url
