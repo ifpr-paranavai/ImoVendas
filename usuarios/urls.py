134fb7d14dec74bceed0091a36bd3d54034bea8d
jgdml <jgdml2801@gmail.com>
2022-06-27 20:49:15 -0300

    Cadastro de usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from .views import PerfilCreate
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/form-login.html'), name='login'),
+    path('cadastro/', PerfilCreate.as_view(
+        template_name='usuarios/form-cadastro.html'), name='cadastro'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from tabnanny import verbose
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+from django.forms import CharField, PasswordInput, ModelForm
+
+from cadastros.models import Perfil
 
 # Create your views here.
+
+class PerfilForm(ModelForm):
+    nome_de_usuario = CharField(help_text="Digite o seu nome de usuário", max_length=80)
+    senha = CharField(widget=PasswordInput, help_text="Digite sua senha")
+    senha_repetida = CharField(widget=PasswordInput, help_text="Repita a senha digitada")
+
+    class Meta:
+        model = Perfil
+        exclude = ["usuario"]
+        
+
+
+class PerfilCreate(CreateView):
+    form_class = PerfilForm
+    template_name = "usuarios/cadastro-form.html"
+    success_url = reverse_lazy('index')
+    model = Perfil
+
+
+    def form_valid(self, form):
+
+        if form.cleaned_data["senha"] == form.cleaned_data["senha_repetida"]:
+            usuario = User.objects.create_user(form.cleaned_data["nome_de_usuario"], form.instance.email, form.cleaned_data["senha"])
+        
+
+        form.instance.usuario = usuario
+
+        url = super().form_valid(form)
+
+        return url
