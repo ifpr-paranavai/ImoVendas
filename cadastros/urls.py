5e2f77fb09243934733db311fac02849701f1e85
jgdml <jgdml2801@gmail.com>
2022-07-04 19:38:48 -0300

    alteração url

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ImovelCreate
 
 urlpatterns = [
-    path('cadastrar/imovel/', ImovelCreate.as_view(template_name='cadastros/imovel-form.html'), name='cadastro-imovel'),
+    path('criar/imovel/', ImovelCreate.as_view(template_name='cadastros/imovel-form.html'), name='criar-imovel'),
 ]
