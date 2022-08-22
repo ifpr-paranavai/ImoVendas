4f76a930d83a17bf8b2d2a8882158bc2e8cd1155
jgdml <jgdml2801@gmail.com>
2022-08-22 20:54:58 -0300

    início ver imóvel

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Index
+from .views import ImovelView, Index
 
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('imovel/<pk>/', ImovelView.as_view(), name='imovel-view'),
 ]
--- paginas/views.py
+++ paginas/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from cadastros.models import Imovel
 
@@ -7,3 +8,8 @@ from cadastros.models import Imovel
 class Index(ListView):
     model = Imovel
     template_name = 'paginas/index.html'
+
+
+class ImovelView(DetailView):
+    model = Imovel
+    template_name = 'paginas/ver_imovel.html'
