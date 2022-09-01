26fbffea95252ce1bc73d9f4a22da7ada39d60be
jgdml <jgdml2801@gmail.com>
2022-09-01 14:25:42 -0300

    logout url

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,5 +4,6 @@ from .views import PerfilCreate
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/form-login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('cadastro/', PerfilCreate.as_view(template_name='usuarios/form-cadastro.html'), name='cadastro'),
 ]
