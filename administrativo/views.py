9db3baa669665de43775810a9065bac9ae410033
jgdml <jgdml2801@gmail.com>
2022-09-22 12:48:40 -0300

    administrativo acesso por grupo

--- administrativo/views.py
+++ administrativo/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from braces.views import GroupRequiredMixin
 
-class Adm(TemplateView):
+class Adm(GroupRequiredMixin, TemplateView):
+    group_required = u'Administrador'
     template_name = "administrativo/index.html"
