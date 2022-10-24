6acb3a62101bc1e9aefaa9df5bdeec557764e713
jgdml <jgdml2801@gmail.com>
2022-10-24 20:25:23 -0300

    autocomplete cidade

--- imovendas/urls.py
+++ imovendas/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('autocomplete.urls')),
     path('', include('paginas.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
