f50966e27f343467b0154a3e63b6d788bf079bd4
jgdml <jgdml2801@gmail.com>
2022-11-09 17:52:43 -0300

    validação tipo de comprovante

--- paginas/forms.py
+++ paginas/forms.py
@@ -1,8 +1,22 @@
 from cadastros.models import Movimentacao
 from django import forms
+from django.core.exceptions import ValidationError
+
+def validate_file_extension(value):
+    ext = value.name.split(".")[-1]
+    exts = ['pdf', 'doc', 'docx', 'jpg', 'png', 'xlsx', 'xls']
+    msg = "O comprovante precisa ser um documento. Documentos aceitos: "
+
+    for e in exts:
+        msg += f"{e} "
+
+    if not ext.lower() in exts:
+        raise ValidationError(msg)
 
 class MovimentacaoForm(forms.ModelForm):
-    comprovante = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), help_text="Insira o comprovante")
+    comprovante = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': False}), help_text="Insira o comprovante", validators=[validate_file_extension])
+
+    
 
     class Meta:
         model = Movimentacao
